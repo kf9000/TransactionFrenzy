2417c54db4d173f64db1449291cb28c3642b3ae0
kf9000 <aapo.kois@gmail.com>
2026-02-19 00:02:15 +0200

    Added barcodescanner

---
+++ Assets/Scripts/KeepWaistInPlace.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class KeepWaistInPlace : MonoBehaviour
+{
+    public Transform xrCamera;     // Assign Main Camera
+    public float waistOffset = 0.7f; // Adjust for player height
+
+    void LateUpdate()
+    {
+        Vector3 camPos = xrCamera.position;
+        transform.position = new Vector3(camPos.x, camPos.y - waistOffset, camPos.z);
+
+        float yaw = xrCamera.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0f, yaw, 0f);
+    }
+}
