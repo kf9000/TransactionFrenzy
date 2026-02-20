3aced09dda38a39a1c3f1ac8a031c5cd199410a6
kf9000 <aapo.kois@gmail.com>
2026-02-21 01:29:16 +0200

    Added raycasts to scanner

--- Assets/Scripts/UseScanner.cs
+++ Assets/Scripts/UseScanner.cs
@@ -6,25 +6,39 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 public class UseScanner : MonoBehaviour
 {
     public XRGrabInteractable grab;
-    public GameObject Ray;
+    public GameObject ray;
+    public GameObject castPoint;
+    public float range = 1;
+    LayerMask ignoredMask;
+
 
     void Awake()
     {
     grab.activated.AddListener(Activated);
     grab.deactivated.AddListener(Deactivated);
+    ignoredMask = LayerMask.GetMask("RayVisual");
 
     }
 
     private void Deactivated(DeactivateEventArgs args)
     {
-        Ray.SetActive(false);
+        ray.SetActive(false);
     }
 
     private void Activated(ActivateEventArgs args)
     {
         if (grab.isSelected)
         {
-            Ray.SetActive(true);
+            ray.SetActive(true);
+
+            if (Physics.Raycast(castPoint.transform.position, castPoint.transform.forward, out RaycastHit hit, range, ~ignoredMask))
+            {
+                if(hit.collider.CompareTag("Barcode"))
+                {
+                    Debug.Log("Barcode hit!");
+                }
+            }
+
         }
     }
 
