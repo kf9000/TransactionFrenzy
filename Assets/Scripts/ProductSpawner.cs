3b88dcc847457ef79e87fe32799a77a427c28389
kf9000 <aapo.kois@gmail.com>
2026-02-23 21:44:19 +0200

    Randomized product rotations

--- Assets/Scripts/ProductSpawner.cs
+++ Assets/Scripts/ProductSpawner.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ProductSpawner : MonoBehaviour
@@ -6,6 +9,36 @@ public class ProductSpawner : MonoBehaviour
 
     public void SpawnProduct()
     {
-        Instantiate(product, transform);
+
+
+        Instantiate(product, transform.position, getRandomRotation());
+  
+    }
+
+    Quaternion getRandomRotation()
+    {
+        Vector3[] axes = {
+            Vector3.up,
+            Vector3.down,
+            Vector3.left,
+            Vector3.right,
+            Vector3.forward,
+            Vector3.back
+        };
+
+        List<Quaternion> rotations = new();
+
+        foreach (Vector3 up in axes)
+        {
+            foreach (Vector3 forward in axes)
+            {
+                if(Vector3.Dot(up, forward) == 0)
+                {
+                    rotations.Add(Quaternion.LookRotation(forward, up));
+                }
+            }
+        }
+
+        return rotations[Random.Range(0, rotations.Count())];
     }
 }
